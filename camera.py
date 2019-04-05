b4f0f7a9cd3c85bceee438c74432fc94326348f3
fredrvaa <fvaagaard@gmail.com>
2019-04-05 20:48:38 +0200

    Added panorama test images

---
+++ camera.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+
+panorama_image = cv2.imread('images/360.jpg')
+
+height, width, channels = panorama_image.shape
+
+print(panorama_image.shape)
+
+size = 150
+
+x_curr = size
+y_curr = size
+
+horizontally_moved = 0
+horizontal = False
+
+last_vertical = 'down'
+vertical_up = False
+vertical_down = True
+
+pred_box = None
+
+while(1):
+    if pred_box == None:
+        if horizontal:
+            x_curr += 1
+            horizontally_moved += 1
+        elif vertical_up:
+            y_curr -= 1
+        elif vertical_down:
+            y_curr += 1
+
+        if y_curr >= height - size and vertical_down:
+            horizontal = True
+            vertical_down = False
+        elif y_curr <= size and vertical_up:
+            horizontal = True
+            vertical_up = False
+        elif horizontally_moved >= 2 * size and horizontal:
+            horizontally_moved = 0
+            horizontal = False
+            if last_vertical == 'up':
+                last_vertical = 'down'
+                vertical_down = True
+            elif last_vertical == 'down':
+                last_vertical = 'up'
+                vertical_up = True
+
+    camera_image = panorama_image[y_curr-size:y_curr+size, x_curr-size:x_curr+size]
+    cv2.imshow('panorama', camera_image)
+    cv2.waitKey(10)
--- dataset_generator/load_voc.py
+++
@@ -1,9 +0,0 @@
-import voc_exporter as ve
-
-labeled_data = "original_dataset/smokes.json" # file path to JSON export with XY paths
-
-# where to write VOC annotation and image outputs
-annotations_output_dir = "original_dataset/Annotations"
-images_output_dir = "original_dataset/JPEGImages"
-
-ve.from_json(labeled_data, annotations_output_dir, images_output_dir, label_format='XY')
